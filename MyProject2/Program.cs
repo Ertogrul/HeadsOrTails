b58931a21128a7d11e12ed0707538b197ecd7cac
Ertogrul <ertogrulhatemoglu@gmail.com>
2022-07-27 14:23:07 +0300

    first commit

---
+++ MyProject2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            for (int i = 1; i <= 10; i++)
+            {
+                int sayi = random.Next(2);
+                Console.WriteLine(i+". Deneme:");
+                if (sayi == 1)
+                
+                    Console.WriteLine("Yazı");
+                else
+                    Console.WriteLine("Tura");
+            }
+            Console.ReadLine();
+        }
+    }
+}
